8ffc4c6b27406cc025b2a33f1aa2f0ecd554a280
unknown <jasskings8503@gmail.com>
2023-03-17 15:59:51 +0530

    today4

---
+++ products.js
@@ -0,0 +1,419 @@
+var productlistval = [{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/Beige_large.png?v=1660370349",
+    Name:"Wave Select Smartwatch",
+    Price:2099,
+    MRP:6900,
+    Percent_off:"80% off",
+    Rating:4.6,
+    Discount:"15 Extra Discount on UPI",
+    Discription:" 1.69(4.29 cm) Colour HD Display, 20+ Active Sport Modes, Heart Rate & SpO2 Monitor, 10 Day Battery Life",
+    Category:"Smart Watch",
+    Status:"Out-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main_blue_759e7ad4-18af-4407-9e8a-91c0058b1a74_300x.png?v=1648108782",
+    Name:"Wave Lite",
+    Price:1499,
+    MRP:6900,
+    Percent_off:"79% off",
+    Rating:4.6,
+    Discount:"15 Extra Discount on UPI",
+    Discription:" 1.69(4.29 cm) Colour HD Display, 20+ Active Sport Modes, Heart Rate & SpO2 Monitor, 10 Day Battery Life",
+    Category:"Smart Watch",
+    Status:"In-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/wave-call-3_300x.png?v=1658295343",
+    Name:"Wave Call",
+    Price:1499,
+    MRP:7990,
+    Percent_off:"80% off",
+    Rating:4.9,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Bluetooth Calling Smartwatch with 1.69 (4.29 cm) HD Curved Display, 150+ Watch Faces, Heart Rate & SPO2 Monitoring",
+    Category:"Smart Watch",
+    Status:"In-Stock",
+},
+// Airpods
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/cream_300x.png?v=1668756103",
+    Name:"Airdopes 131",
+    Price:999,
+    MRP:2990,
+    Percent_off:"67% off",
+    Rating:4.8,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Wireless Earbuds with upto 60 Hours Playback, 13mm Drivers and IWP Technology, 650mah C type Charging Case",
+    Category:"Wireless-Earbuds",
+    Status:"In-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/grey_600x.png?v=1668599490",
+    Name:"Airdopes 141",
+    Price:1299,
+    MRP:4490,
+    Percent_off:"79% off",
+    Rating:4.8,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Equipped with 8mm drivers, Upto 42 Hours Total Playback, ENx™ Technology, ASAPTM Charge",
+    Category:"Wireless-Earbuds",
+    Status:"In-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main_white_9263689b-ad66-47cd-8183-f59685e1dff6_600x.png?v=1671193643",
+    Name:"Airdopes 163",
+    Price:1399,
+    MRP:2490,
+    Percent_off:"44% off",
+    Rating:4.8,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Wireless Earbuds with Massive Playback of upto 17 Hour, IPX5 Water & Sweat Resistance, IWP Technology, Type C Interface",
+    Category:"Wireless-Earbuds",
+    Status:"In-Stock",
+},
+//NeckBands
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/rockerz-235-v2-red_de759211-e939-4ce9-9a77-c68705104ea7_600x.png?v=1667648184",
+    Name:"TRebel Rockerz 235 V2",
+    Price:1199,
+    MRP:2990,
+    Percent_off:"60% off",
+    Rating:4.8,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Wireless Earphone with 10mm Drivers, 8 Hours Non-Stop Music, Single Command Assist, Fast Charging",
+    Category:"NeckBands",
+    Status:"In-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/Untblueitled-1_600x.png?v=1661509030",
+    Name:"Rockerz 103 Pro",
+    Price:899,
+    MRP:2490,
+    Percent_off:"64% off",
+    Rating:4.9,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Wireless Earphones with 10 mm Drivers, Single Press Voice Assistant, Up To 20 hrs Uninterrupted, ENx™ Technology",
+    Category:"NeckBands",
+    Status:"In-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/35ca6350-d6f3-4bc2-a742-01b2c88d80c4_800x.png?v=1673005509",
+    Name:"Rockerz 255 Pro+",
+    Price:1299,
+    MRP:3990,
+    Percent_off:"67% off",
+    Rating:4.8,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Bluetooth Earphone with 10mm drivers, Up to 40H Nonstop Playback, Type-C charging, ASAP Charge",
+    Category:"NeckBands",
+    Status:"In-Stock",
+},
+//Wireless Headphones
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/rockerz-518-blue_600x.png?v=1613731627",
+    Name:"Rockerz 518",
+    Price:1299,
+    MRP:3990,
+    Percent_off:"67% off",
+    Rating:4.9,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Wireless Headphone with Dynamic 50mm Driver, Up To 20 Hours Of Playtime, Easy Control and Lightweight",
+    Category:"Wireless-HeadPhones",
+    Status:"In-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/c9b304e6-1451-4ac7-95a1-5aa1cf51df05_45954dbd-6b69-4ae5-bed5-27cb1780d391_300x.png?v=1629538797",
+    Name:"Rockerz 558",
+    Price:1999,
+    MRP:2999,
+    Percent_off:"60% off",
+    Rating:5.0,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"boAt Rockerz 558",
+    Category:"Wireless-HeadPhones",
+    Status:"In-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/06bd8915-8db7-4575-9558-c97bf050d8d7_600x.png?v=1625044994",
+    Name:"Rockerz 400",
+    Price:1499,
+    MRP:2990,
+    Percent_off:"50% off",
+    Rating:4.8,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Wireless Bluetooth Headphone with 40mm Dyanmic Drivers, 8 Hours Non-Stop Playback, Lightweight & Portable",
+    Category:"Wireless-HeadPhones",
+    Status:"In-Stock",
+},
+//Wireless Speakers
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main_250_black_600x.png?v=1673001721",
+    Name:"Stone 250",
+    Price:1499,
+    MRP:3990,
+    Percent_off:"62% off",
+    Rating:4.8,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"5W Portable Wireless Speaker with RGB LED, 9 Hours of Playtime, IPX7 Water Resistant",
+    Category:"Wireless-Speakers",
+    Status:"In-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/3_6f20b409-43b2-43d5-96ac-e9f8a26d79a8_600x.png?v=1658436783",
+    Name:"Stone 1000v2",
+    Price:3499,
+    MRP:6990,
+    Percent_off:"50% off",
+    Rating:5.0,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"14W Powerful Stereo Sound, Up to 8hrs Nonstop Playback, Travel & Party Booster, 3000mah Battery",
+    Category:"Wireless-Speakers",
+    Status:"In-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/74a6b20d-9842-49da-b279-022812b81e1f_600x.png?v=1673001663",
+    Name:"Stone 350",
+    Price:1699,
+    MRP:3490,
+    Percent_off:"51% off",
+    Rating:4.8,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Wireless Speaker with 10W Stereo Sound, 12H Nonstop Playtime, Lightweight Design, BT, TF Card & AUX Compatible",
+    Category:"Wireless-Speakers",
+    Status:"In-Stock",
+},
+//Smart Watch-2
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/WaveCall.197_d6cce9fa-8242-443a-b1e1-0fefdd1751c8_300x.png?v=1678190676",
+    Name:"TRebel Wave Call",
+    Price:1799,
+    MRP:7990,
+    Percent_off:"77% off",
+    Rating:4.6,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"TRebel Wave Call",
+    Category:"Smart Watch",
+    Status:"In-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main1_f30681d9-fddb-4997-bfcb-503fcc258a5c_600x.png?v=1646392933",
+    Name:"Matrix",
+    Price:1799,
+    MRP:11990,
+    Percent_off:"85% off",
+    Rating:4.6,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"1.65 (4.18 cm) colour AMOLED Display, Fitness Tracking, 100+ Cloud Watch Faces",
+    Category:"Smart Watch",
+    Status:"In-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/Wavebeatcall1.2_700x.png?v=1674561165",
+    Name:"Wave Beat Call",
+    Price:1799,
+    MRP:7990,
+    Percent_off:"79% off",
+    Rating:4.9,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Bluetooth Calling Smart Watch with 1.69 (4.29 cm) HD Display, 600+ Cloud Watch faces, Live",
+    Category:"Smart Watch",
+    Status:"Out-Stock",
+},
+//Airpods-2
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main_grey_8202f335-95e8-4bc4-9889-ea9beb8292e4_300x.png?v=1664103052",
+    Name:"Airdopes 183 ",
+    Price:1599,
+    MRP:2990,
+    Percent_off:"47% off",
+    Rating:4.8,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Wireless Bluetooth Earbuds with 10mm Driver, Upto 90 min Playback in 10 min Charge, ASAP Charge Technology, BEAST™ mode",
+    Category:"Wireless-Earbuds",
+    Status:"Out-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/airdopes-121-v2-pink_600x.png?v=1678773275",
+    Name:"Airdopes 121 v2",
+    Price:1199,
+    MRP:2999,
+    Percent_off:"60% off",
+    Rating:4.8,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"In-Ear Earbuds v2 with 8mm driver, LED Case Battery Indicator, 380mAh Pocket Friendly Charging",
+    Category:"Wireless-Earbuds",
+    Status:"In-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main1_c5f9fe7c-c012-4d93-ba0c-b25267c0560d_300x.png?v=1647426797",
+    Name:"Airdopes 411 ANC",
+    Price:1999,
+    MRP:4990,
+    Percent_off:"60% off",
+    Rating:4.8,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Noise-Cancelling Earbuds with 10mm Drivers, ASAPTM Charge Technology, Up to 25dB ANC, ENx™ Technology, 17.5 Hours Playback",
+    Category:"Wireless-Earbuds",
+    Status:"In-Stock",
+},
+//Neckbands-2
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main3_e0d541c5-72de-4f54-a19b-1c324082742a_600x.png?v=1654195093",
+    Name:"Rockerz 260",
+    Price:1299,
+    MRP:2499,
+    Percent_off:"48% off",
+    Rating:4.9,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Wireless Bluetooth Earphone with Insta Wake & Pair, Quad Mics with ENx Technology & Asap Charge",
+    Category:"NeckBands",
+    Status:"In-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/245pro3_700x.png?v=1663584969",
+    Name:"Rockerz 103 Pro",
+    Price:899,
+    MRP:2490,
+    Percent_off:"64% off",
+    Rating:4.9,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Wireless Earphones with 10 mm Drivers, Single Press Voice Assistant, Up To 20 hrs Uninterrupted, ENx™ Technology",
+    Category:"NeckBands",
+    Status:"In-Stock",
+},
+{
+    Image:"https://cdn.shopify.com/s/files/1/0057/8938/4802/products/245pro4_700x.png?v=1663584969",
+    Name:"Rockerz 245 Pro",
+    Price:1299,
+    MRP:2499,
+    Percent_off:"48% off",
+    Rating:4.8,
+    Discount:"15 Extra Discount on UPI",
+    Discription:"Wireless Earphones with 10 min ASAP™ Fast Charge, 20 Hours of nonstop playback & ENx™ Technology",
+    Category:"NeckBands",
+    Status:"Out-Stock",
+},
+]
+displayproducts(productlistval);
+function displayproducts(list){
+    document.querySelector("#parent").textContent="";
+    list.map(function(elem,index){
+        var div = document.createElement("div");
+        div.setAttribute("class","child");
+
+        var div1=document.createElement("div");
+        div1.setAttribute("class","image");
+
+        var image = document.createElement("img");
+        image.setAttribute("src",elem.Image);
+        image.setAttribute("alt",elem.Name);
+
+        div1.append(image);
+
+        var div2 = document.createElement("div");
+        div2.setAttribute("class","details");
+
+        var anch1 = document.createElement("a");
+        anch1.setAttribute("href","hello.html");
+        anch1.setAttribute("class","product_name");
+        anch1.textContent = elem.Name;
+        anch1.addEventListener("click",function(){
+            details(elem);
+        });
+
+        var div3 = document.createElement("div");
+        div3.setAttribute("class","price_details")
+
+        var h42 = document.createElement("h4");
+        h42.setAttribute("class","price");
+        h42.textContent = elem.Price;
+
+        var h43 = document.createElement("h4");
+        h43.setAttribute("class","percentoff");
+        h43.textContent = elem.Percent_off;
+
+        var h44 = document.createElement("h4");
+        h44.textContent = elem.MRP;
+        
+        div3.append(h42,h43,h44);
+
+        var div4 = document.createElement("div");
+        div4.setAttribute("class","reviews");
+
+        var p1 = document.createElement("p");
+        p1.setAttribute("class","product_rating");
+        p1.textContent = elem.Rating;
+
+        div4.append(p1);
+
+        var p2 = document.createElement("p");
+        p2.textContent = elem.Discount;
+
+        var btn;
+        
+        if(elem.Status === "In-Stock"){
+            btn = document.createElement("button");
+            btn.setAttribute("class","acbtn");
+            btn.textContent = "ADD TO CART";
+            btn.addEventListener("click",function(){
+            addtocart(elem);
+           })
+        }
+        else{
+            btn = document.createElement("div");
+            btn.setAttribute("class","nmdiv");
+            var nmbtn = document.createElement("a");
+            nmbtn.setAttribute("href","hello.html");
+            nmbtn.setAttribute("class","nmanchor");
+            nmbtn.textContent = "NOTIFY ME";
+            nmbtn.addEventListener("click",function(){
+                notifyme(elem);
+            })
+            btn.append(nmbtn);
+        }
+
+        div2.append(anch1,div3,div4,p2,btn);
+        div.append(div1,div2);
+        document.querySelector("#parent").append(div);
+
+    })
+}
+// Product Details Function
+var productdetails=JSON.parse(localStorage.getItem("product_details")) || [];
+function details(elem){
+    // alert("clicked");
+    productdetails.push(elem);
+    localStorage.setItem("product_details",JSON.stringify(productdetails));
+}
+// Add To Cart Function
+var cartitems = JSON.parse(localStorage.getItem("cart_list")) || [];
+function addtocart(elem){
+    alert("clicked");
+    cartitems.push(elem);
+    localStorage.setItem("cart_list",JSON.stringify(cartitems));
+}
+//Notify Me 
+function  notifyme(elem){
+    alert("Notify Me");
+}
+//Filter Function
+function filterproducts(){
+    var selected = document.getElementById("showfilters").value; 
+    var h1 = document.querySelector("h1");
+    if(selected === ""){
+        h1.textContent = "Our Products";
+    }
+    else{
+        h1.textContent = selected;
+    }
+    var filtered = productlistval.filter(function(elem){
+        if(selected === ""){
+            return true;
+        }
+        else{
+            
+            return elem.Category === selected;
+        }
+    })
+    displayproducts(filtered);
+}
